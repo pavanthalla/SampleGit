41a4bbdc6c57ac625beca1ba3491ef719929a494
pavanthalla <pavanthalla012@gmail.com>
2020-06-28 12:54:34 +0530

    Message sample

--- search_in_flutter/android/app/src/main/kotlin/com/example/searchinflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.searchinflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tabs_flutter/android/app/src/main/kotlin/com/example/tabsflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tabsflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
